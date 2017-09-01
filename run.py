4cdcfcd0ee9f4513d6622be81e3b8baf935d94ef
raw-at <abhinavrawat728@gmail.com>
2017-09-01 16:15:49 +0530

    Everything done for linux gui

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import sys
 import glob
 
 from vispy import app, visuals, scene
+from vispy.scene import ViewBox
 from vispy.scene.visuals import Markers, Line, XYZAxis
 import vispy
 
@@ -11,6 +12,7 @@ vispy.app.use_app(backend_name="PyQt5", call_reuse=True)
 import numpy as np
 
 import mesh_lib
+
 
 class canvasCreater:
     def load_config(self):
@@ -28,16 +30,14 @@ class canvasCreater:
                 measurements.append(line.split())
         return measurements
 
+    def debug(self, measurements, file_path, add_geodesic):
 
-    def debug(self,measurements, file_path, add_geodesic):
-        
         # Model object
         model = mesh_lib.Model(file_path)
         model_point_coordinates = model.get_coords()
 
-        canvas = scene.SceneCanvas(keys='interactive', show=True)
+        canvas = scene.SceneCanvas(keys='interactive')
         view = canvas.central_widget.add_view()
-    
 
         # all model - GREEN
         points = Markers(parent=view.scene)
@@ -47,12 +47,12 @@ class canvasCreater:
             face_color=(0, 1, 0, .3),
             size=5
         )
-
-        for m in measurements: # measurements in config file
+        data_list = []
+        for m in measurements:  # measurements in config file
             # parsing key vertexes and description text
-            point_1 = int(m[1])+1
-            point_2 = int(m[2])+1
-            point_3 = int(m[3])+1
+            point_1 = int(m[1]) + 1
+            point_2 = int(m[2]) + 1
+            point_3 = int(m[3]) + 1
             text = " ".join(m[4:])
 
             # coordinates of key vertexes
@@ -61,13 +61,13 @@ class canvasCreater:
             plane = mesh_lib.get_plane(key_coords)
 
             # key vertexes WHITE
-            points = Markers(parent=view.scene)
+            points = Markers()
             points.set_data(
                 pos=key_coords,
                 edge_color=None,
                 face_color=(1, 1, 1, 1),
                 size=5
-            )        
+            )
 
             # "C" - circumference
             if m[0] == "C":
@@ -76,14 +76,14 @@ class canvasCreater:
                 p_2 = model.get_path(point_2, point_3)
                 p_3 = model.get_path(point_3, point_1)
                 # full path
-                path = p_1+p_2[1:]+p_3[1:]
+                path = p_1 + p_2[1:] + p_3[1:]
             # "L" - Length
             if m[0] == "L":
                 # 2 segments of path (indexes)
                 p_1 = model.get_path(point_1, point_2)
                 p_2 = model.get_path(point_2, point_3)
                 # full path
-                path = p_1+p_2[1:]
+                path = p_1 + p_2[1:]
 
             # geodesic
             geodesic_coordinates = model.get_coords(path)
@@ -91,25 +91,29 @@ class canvasCreater:
             print("{0}:".format(text))
             print(
                 "  Geodesic distance: {0} cm".format(
-                round(100*geodesic_length, 3)
+                    round(100 * geodesic_length, 3)
                 )
             )
-            if add_geodesic: # if debug_full
+
+            if add_geodesic:  # if debug_full
                 # geodesic line - RED
                 line = Line(parent=view.scene)
                 line.set_data(
                     pos=geodesic_coordinates,
                     color=(1, 0, 0, 1)
                 )
-            
+
             # approximated
-            flattened_coordinates = mesh_lib.get_projections(plane, geodesic_coordinates)  
+            flattened_coordinates = mesh_lib.get_projections(plane, geodesic_coordinates)
             flattened_length = mesh_lib.get_length(flattened_coordinates)
             print(
                 "  Approximated distance: {0} cm".format(
-                round(100*flattened_length, 3)
+                    round(100 * flattened_length, 3)
                 )
             )
+            
+            data_list.append(geodesic_length)
+            data_list.append(flattened_length)
             # flattened line - BLUE
             line = Line(parent=view.scene)
             line.set_data(
@@ -122,18 +126,22 @@ class canvasCreater:
         view.camera.distance = 3
 
         axis = XYZAxis(parent=view.scene)
-        return view
-        #app.run()
+        final_result = {"canvas":canvas,"data":data_list}
+        return final_result
+        # app.run()
 
-    def calculate_all(self,measurements, models_folder, results_folder): # AAAAAAAllll of this
+    def calculate_all(self, measurements, models_folder, results_folder):  # AAAAAAAllll of this
+        
+        
         # for each *.obj file in models_folder
+        model_data = list()
         for model_path in glob.glob("{0}/*.obj".format(models_folder)):
 
             # creating model object
             model = mesh_lib.Model(model_path)
 
             # parsing the filenames
-            model_name = model_path.split("\\")[1].split(".")[0]
+            model_name = model_path.split("/")[1].split(".")[0]
             results_file = "{0}/{1}.txt".format(results_folder, model_name)
 
             # forming the results file
@@ -141,11 +149,11 @@ class canvasCreater:
 
                 ### Segment almost identical to debug function
 
-                for m in measurements: 
+                for m in measurements:
 
-                    point_1 = int(m[1])+1
-                    point_2 = int(m[2])+1
-                    point_3 = int(m[3])+1
+                    point_1 = int(m[1]) + 1
+                    point_2 = int(m[2]) + 1
+                    point_3 = int(m[3]) + 1
                     text = " ".join(m[4:])
 
                     key_coords = model.get_coords((point_1, point_2, point_3))
@@ -156,27 +164,31 @@ class canvasCreater:
                         p_2 = model.get_path(point_2, point_3)
                         p_3 = model.get_path(point_3, point_1)
 
-                        path = p_1+p_2[1:]+p_3[1:]
+                        path = p_1 + p_2[1:] + p_3[1:]
 
                     if m[0] == "L":
                         p_1 = model.get_path(point_1, point_2)
                         p_2 = model.get_path(point_2, point_3)
 
-                        path = p_1+p_2[1:]
-                        
+                        path = p_1 + p_2[1:]
+
                     geodesic_coordinates = model.get_coords(path)
                     flattened_coordinates = mesh_lib.get_projections(
                         plane,
                         geodesic_coordinates
-                        )
+                    )
                     flattened_length = mesh_lib.get_length(flattened_coordinates)
                     # output that is going to results file is formed here:
-                    output = "{0}: {1} cm.\n".format(text, round(100*flattened_length, 3))
+                    output = "{0}: {1} cm.\n".format(text, round(100 * flattened_length, 3))
 
                     # writing to file
                     fout.write(output)
+                    print(model_name)
+            model_data.append(model_name)
             # progress displayed by print messages
             print("{0}.txt finished.".format(model_name))
+                    
+        return model_data 
 
 if __name__ == "__main__":
 
@@ -190,9 +202,9 @@ if __name__ == "__main__":
 
     # debug or all file calculations
     if len(sys.argv) == 3 and sys.argv[1] == "debug":
-        canvasObj.debug(measurements, "{0}/{1}".format(m_f,sys.argv[2]), False)
+        canvasObj.debug(measurements, "{0}/{1}".format(m_f, sys.argv[2]), False)
     elif len(sys.argv) == 3 and sys.argv[1] == "debug_full":
-        canvasObj.debug(measurements, "{0}/{1}".format(m_f,sys.argv[2]), True)
+        canvasObj.debug(measurements, "{0}/{1}".format(m_f, sys.argv[2]), True)
     elif len(sys.argv) == 1:
         canvasObj.calculate_all(measurements, m_f, r_f)
     else:
--- work.py
+++ work.py
@@ -1,30 +1,31 @@
 import sys
+from functools import partial
 from shutil import copyfile
-import os
-#import vispy.mpl_plot as plt
-import subprocess as sp
+
+# import vispy.mpl_plot as plt
 import vispy.app
 
 vispy.app.use_app(backend_name="PyQt5", call_reuse=True)
 
-#here we import canvasCreater call from run.py file
+# here we import canvasCreater call from run.py file
 from run import canvasCreater
 
-#from PyQt5.QtWidgets import (QApplication,QFrame, QCheckBox,QGridLayout, QHBoxLayout, QPushButton, QSizePolicy, QSpacerItem, QToolButton, QVBoxLayout, QWidget,
-#QGridLayout,QFileDialog,QRadioButton,QMainWindow)
-from PyQt5.QtWidgets import *
+# from PyQt5.QtWidgets import (QApplication,QFrame, QCheckBox,QGridLayout, QHBoxLayout, QPushButton, QSizePolicy, QSpacerItem, QToolButton, QVBoxLayout, QWidget,
+# QGridLayout,QFileDialog,QRadioButton,QMainWindow)
 
-class Widget(QWidget,canvasCreater):
- 
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Widget(QWidget, canvasCreater):
     filenames_list = []
-    
+
     def __init__(self, parent=None):
         super(Widget, self).__init__(parent=parent)
         self.measurements = self.load_config()
         self.m_f = "models"
         self.r_f = "results"
         self.layoutUI()
-        
 
     def layoutUI(self):
         self.setStyleSheet("background-color: white;")
@@ -35,21 +36,21 @@ class Widget(QWidget,canvasCreater):
         self.leftFrame.setFrameShape(QFrame.StyledPanel)
         self.leftFrame.setFrameShadow(QFrame.Raised)
         self.verticalLayout = QVBoxLayout(self.leftFrame)
+        #self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setSpacing(0)
 
         self.principalLayout.addWidget(self.leftFrame)
-    
 
-        
         self.verticalLayoutR = QVBoxLayout()
-        #self.verticalLayoutR.setSpacing(0)
+        # self.verticalLayoutR.setSpacing(0)
 
         self.keyFrame = QFrame(self)
-        #self.keyFrame.setStyleSheet("background-color: red;")
+        # self.keyFrame.setStyleSheet("background-color: red;")
         self.keyFrame.setFrameShape(QFrame.StyledPanel)
         self.keyFrame.setFrameShadow(QFrame.Raised)
-        
+
         self.keysverticalLayout = QVBoxLayout(self.keyFrame)
-        
+
         self.AddBtn = QPushButton("Add", self.keyFrame)
         self.SelectBtn = QPushButton("Select", self.keyFrame)
         self.RunBtn = QPushButton("Run", self.keyFrame)
@@ -63,30 +64,28 @@ class Widget(QWidget,canvasCreater):
         self.keysverticalLayout.addWidget(self.SelectBtn)
         self.SelectBtn.clicked.connect(self.SelectItem)
 
-
         #################################################
         self.viewFrame = QFrame(self)
         self.viewFrame.setFrameShape(QFrame.StyledPanel)
         self.viewFrame.setFrameShadow(QFrame.Raised)
         self.gridLayout = QGridLayout(self.viewFrame)
-        
-        #self.verticalLayout.addStretch(1)
+
+        # self.verticalLayout.addStretch(1)
         self.cb = QCheckBox(self.viewFrame)
-        self.cb.stateChanged.connect(self.checkBox)
+        #self.cb.stateChanged.connect(self.checkBox)
         ###################################################
         self.ViewBtn = QPushButton("View", self.viewFrame)
         self.ViewBtn.clicked.connect(self.ViewItem)
 
-        
-        self.gridLayout.addWidget(self.cb,0,0)
-        self.gridLayout.addWidget(self.ViewBtn,0,1)
-        
-        #self.ViewBtn.move(0,50)
-        
-        
+        self.gridLayout.addWidget(self.cb, 0, 0)
+        self.gridLayout.addWidget(self.ViewBtn, 0, 1)
+
+        # self.ViewBtn.move(0,50)
+
+
         self.keysverticalLayout.addWidget(self.viewFrame)
-    
-        #self.keysverticalLayout.addWidget(self.ViewBtn)
+
+        # self.keysverticalLayout.addWidget(self.ViewBtn)
         #####################################################
         self.keysverticalLayout.addWidget(self.RunBtn)
         self.RunBtn.clicked.connect(self.RunItem)
@@ -95,70 +94,135 @@ class Widget(QWidget,canvasCreater):
         self.keysverticalLayout.addWidget(self.StopBtn)
         self.StopBtn.clicked.connect(self.StopItem)
 
-        
-        
         self.verticalLayoutR.addWidget(self.keyFrame)
+        self.verticalLayoutR.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayoutR.setSpacing(0)
+
         self.principalLayout.addLayout(self.verticalLayoutR)
-
-        
-
 
         self.rightFrame = QFrame(self)
         self.rightFrame.setFrameShape(QFrame.StyledPanel)
         self.rightFrame.setFrameShadow(QFrame.Raised)
         self.verticalLayoutRight = QVBoxLayout(self.rightFrame)
+        self.verticalLayoutRight.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayoutRight.setSpacing(0)
 
         self.principalLayout.addWidget(self.rightFrame)
-    
-        
-    
+
     def AddItem(self):
         file_name_list = []
         fname = QFileDialog.getOpenFileName(self, 'Open file', '/')
-        
+
         file_selected = str(fname[0])
-        #print(file_selected)
+        # print(file_selected)
         file_name_list = file_selected.split('/')
-        
+
         final_file_name = file_name_list[-1]
         Widget.filenames_list.append(final_file_name)
-        
-        path = str(fname[0])
- 
-        dst = "/home/raw-at/Desktop/pyqt5/models/"+final_file_name
 
-        
-        #print(final_file_name)
+        path = str(fname[0])
+
+        dst = "/home/raw-at/Desktop/pyqt5_gui/models/" + final_file_name
+
+        # print(final_file_name)
 
         copyfile(path, dst)
 
-            
     def SelectItem(self):
-        
-        
         for i in range(len(Widget.filenames_list)):
             print(i)
-            name = "obj"+str(i)
-    
-            self.name = QRadioButton(self.filenames_list[i],self.rightFrame) 
-            self.verticalLayoutRight.addWidget(self.name)
-    
-    
+            name = "obj" + str(i)
 
-        
+            self.name = QRadioButton(self.filenames_list[i], self.rightFrame)
+            self.verticalLayoutRight.addWidget(self.name)
+
+
     def ViewItem(self):
-        #sp.Popen("python3 run.py debug 1.obj",shell=True)
-        #self.debug()
+        # sp.Popen("python3 run.py debug 1.obj",shell=True)
+        # self.debug()
+        # here we call the debug function of the imported class
+        if self.cb.isChecked():
+
+            for i in range(len(Widget.filenames_list)):
+                name = "obj"+str(i)
+
+                if self.name.isChecked():
+                    req_res = self.debug(self.measurements, "models/"+Widget.filenames_list[i], True)
+                    canvas = req_res['canvas']
+                    
+                    canvas.create_native()
+                    canvas.native.setMinimumSize(320, 240)
+                    self.verticalLayout.addWidget(canvas.native)
+                    data = req_res['data']
+
+                    for i in reversed(range(self.verticalLayoutRight.count())): 
+                        self.verticalLayoutRight.itemAt(i).widget().setParent(None)
+
+                    #self.verticalLayoutRight.addWidget(data)
+
+
+                    #print(data)
+                    self.label_1 = QLabel("Head: Geodesic: "+str(data[0]), self)
+                    self.label_2 = QLabel("Head: Approximately: "+str(data[1]), self)
+                    self.label_3 = QLabel("Neck: Geodesic: "+str(data[2]), self)
+                    self.label_4 = QLabel("Neck: Approximately: "+str(data[3]), self)
+                    
+                    self.verticalLayoutRight.addWidget(self.label_1)
+                    self.verticalLayoutRight.addWidget(self.label_2)
+                    self.verticalLayoutRight.addWidget(self.label_3)
+                    self.verticalLayoutRight.addWidget(self.label_4)
+            
         
-        #here we call the debug function of the imported class
-        view = self.debug(self.measurements,"models/1.obj", False)
         
-        #here i just want to embed the output genereated by the self.debug to
-        #Put into the right QFrame of the Gui 
-        #see debug funtion in run.py for other references
+        
+        
+        
+        else:
+
+            for i in range(len(Widget.filenames_list)):
+                name = "obj"+str(i)
+
+                if self.name.isChecked():
+                    req_res = self.debug(self.measurements, "models/"+Widget.filenames_list[i], False)
+                    canvas = req_res['canvas']
+                    
+                    canvas.create_native()
+                    canvas.native.setMinimumSize(320, 240)
+                    self.verticalLayout.addWidget(canvas.native)
+                    data = req_res['data']
+
+                    for i in reversed(range(self.verticalLayoutRight.count())): 
+                        self.verticalLayoutRight.itemAt(i).widget().setParent(None)
+
+                    #self.verticalLayoutRight.addWidget(data)
+
+
+                    #print(data)
+                    self.label_1 = QLabel("Head: Geodesic: "+str(round(data[0]*100,3)), self)
+                    self.label_2 = QLabel("Head: Approximately: "+str(round(data[1]*100,3)), self)
+                    self.label_3 = QLabel("Neck: Geodesic: "+str(round(data[2]*100,3)), self)
+                    self.label_4 = QLabel("Neck: Approximately: "+str(round(data[3]*100,3)), self)
+                    
+                    self.verticalLayoutRight.addWidget(self.label_1)
+                    self.verticalLayoutRight.addWidget(self.label_2)
+                    self.verticalLayoutRight.addWidget(self.label_3)
+                    self.verticalLayoutRight.addWidget(self.label_4)
+
+
+        # here i just want to embed the output genereated by the self.debug to
+        # Put into the right QFrame of the Gui
+        # see debug funtion in run.py for other references
         
     def RunItem(self):
-        pass
+
+            modal_name = self.calculate_all(self.measurements,self.m_f,self.r_f)
+            for i in reversed(modal_name):
+                self.label = QLabel(i+".txt finished", self)
+                self.verticalLayout.addWidget(self.label)
+            
+                                
+
+                        
 
     def StopItem(self):
         print('Stop button')
@@ -166,26 +230,27 @@ class Widget(QWidget,canvasCreater):
     def checkBox(self):
         print("Check Box")
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = Widget()
-    
-    #measurements = w.load_config()
+
+    # measurements = w.load_config()
 
     # folder names
-    #m_f = "models"
-    #r_f = "results"
+    # m_f = "models"
+    # r_f = "results"
 
     w.show()
     sys.exit(app.exec_())
-    
+
     # getting info from config file
-    #canvasObj = canvasCreater()
-    #measurements = w.load_config()
+    # canvasObj = canvasCreater()
+    # measurements = w.load_config()
 
     # folder names
-    #m_f = "models"
-    #r_f = "results"
+    # m_f = "models"
+    # r_f = "results"
 
     # debug or all file calculations
     '''
@@ -198,6 +263,3 @@ if __name__ == '__main__':
     else:
         print("Wrong input format, refer to instruction manual.")
     '''
-
-
-
